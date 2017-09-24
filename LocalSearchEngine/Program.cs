05b43b683b631cb1436d1eda39b59e5065b4837e
Jamie Lord <me@jamielord.co.uk>
2017-09-24 11:06:29 +0100

    Initial solution with .NET Core port of hOOt

---
+++ LocalSearchEngine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace LocalSearchEngine
+{
+    class Program
+    {
+        private static readonly string _workingDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+        static void Main(string[] args)
+        {
+            var hoot = new Hoot.Hoot($"{_workingDir}/Indexes", "test", false);
+
+            hoot.FreeMemory();
+
+            hoot.Index(0, "this is some great text!");
+
+            hoot.OptimizeIndex();
+        }
+    }
+}
